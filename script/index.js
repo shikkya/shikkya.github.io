389aea30d2758591c5375d2ebb4d3238530d1242
zhengy <zhengy@jufaanli.com>
2019-03-12 17:02:46 +0800

    a

---
+++ script/index.js
@@ -0,0 +1,26 @@
+﻿/**
+ * @authors kk
+ * @date    2019-03-12
+ */
+
+var TypeItem = {
+    props: ['content', 'index'],
+    template: '<div :class="\'type type_\'+index"><img :src="\'./images/\'+content.imgName"/><a :href="content.url">{{content.text}}</a></div>',
+    methods: {}
+}
+
+var vm = new Vue({
+    el: '#typeOut',
+    components: {
+        TypeItem: TypeItem
+    },
+    data: {
+        list: [
+            { imgName: 'index_11.png', text: '可可炸飞机', url: '' },
+            { imgName: 'index_05.png', text: '可可美记', url: '' },
+            { imgName: 'index_06.png', text: '倒计时', url: '' },
+            { imgName: 'index_07.png', text: '圆周率', url: '' }
+        ]
+    },
+    methods: {}
+})
