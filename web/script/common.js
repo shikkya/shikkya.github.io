6aa603af0af4943058881ef53f425d872405d696
zhengy <zhengy@jufaanli.com>
2019-05-10 17:35:54 +0800

    a

--- web/script/common.js
+++ web/script/common.js
@@ -3,6 +3,42 @@
  * @date    2019-03-18
  */
 
+var ieVersion = IEVersion();
+alert(ieVersion);
+if (typeof(ieVersion) == 'number' && ieVersion > 0 && ieVersion < 11) {
+    // alert('IE浏览器版本过低，可能无法正常浏览，请尽快升级~');
+}
+
+// 浏览器判断
+function IEVersion() {
+    var userAgent = navigator.userAgent; // 取得浏览器的userAgent字符串  
+    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; // 判断是否IE<11浏览器  
+    var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; // 判断是否IE的Edge浏览器  
+    var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+    if (isIE) {
+        var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+        reIE.test(userAgent);
+        var fIEVersion = parseFloat(RegExp["$1"]);
+        if (fIEVersion == 7) {
+            return 7;
+        } else if (fIEVersion == 8) {
+            return 8;
+        } else if (fIEVersion == 9) {
+            return 9;
+        } else if (fIEVersion == 10) {
+            return 10;
+        } else {
+            return 6; // IE版本<=7
+        }
+    } else if (isEdge) {
+        return 'edge'; // edge
+    } else if (isIE11) {
+        return 11; // IE11  
+    } else {
+        return -1; // 不是ie浏览器
+    }
+}
+
 // 各产品首页右上角导航
 var productList = [
     { text: '可可导航', url: '../index.html' },
@@ -93,22 +129,3 @@ function loadImg() {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-// 页脚
-var vmFooter = new Vue({
-    el: '#footer',
-    template: '<div id="footer" class="footer" v-cloak><ul><li><img :src="logoImg"/></li><li><p>联系 QQ：125048224</p><p>意见反馈：shikkya@qq.com</p></li></ul></div>',
-    data: {
-        logoImg: '../../images/footer_01.png'
-    }
-})
