e709386ac3132df7e82f0387874ddb0dd10350a4
zhengy <zhengy@jufaanli.com>
2019-07-13 16:41:18 +0800

    a

--- web/script/code/common.js
+++ web/script/code/common.js
@@ -1,9 +1,16 @@
 ﻿/**
  * @authors kk
- * @date    2019-05-15
+ * @date    2019-07-13
  */
+
+
+/*
+{ id: '001', tit: '纯CSS实现鼠标经过图片特效炫酷切换动画', type: '影视,明星', intro: '', keyWord: '聂倾城,李一桐', chrome: '', firefox: '', se360: '', tencent: '', sogou: '', opera: '', safari: '', ie: '' }
+ */
+
 
 // 所有数据
 var picList = [
-    { id: '001', tit: '纯CSS实现鼠标经过图片特效炫酷切换动画', type: '影视,明星', intro: '', keyWord: '聂倾城,李一桐', chrome: '', firefox: '', se360: '', tencent: '', sogou: '', opera: '', safari: '', ie: '' }
+    { id: '001', tit: '容器内拖拽分割线自由分配左右空间', type: '其他', keyWord: '拖拽,js', intro: '说明' },
+    { id: '002', tit: '这是一条测试数据', type: '其他', keyWord: '测试', intro: '说明' }
 ];
