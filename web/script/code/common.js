4089e6ed4cc5e873380ed4d151dd52a2d6f09bd4
zhengy <zhengy@jufaanli.com>
2019-08-10 15:27:33 +0800

    a

--- web/script/code/common.js
+++ web/script/code/common.js
@@ -16,6 +16,6 @@ var PicItem = {
 
 // 所有数据 [ chrome, firefox, safari, opera, tencent, se360, ie8, ie11 ]  [ Android, Ios, WeChat ]
 var picList = [
-    { id: '001', tit: '容器内拖拽分割线自由分配左右空间', type: '其他', keyWord: ['拖拽', 'js'], intro: '', compatible: ['', '1', '1', '0', '', '1', '', '0'], comMobile: ['', '', ''] },
+    { id: '001', tit: '容器内拖拽分割线自由分配左右空间', type: '其他', keyWord: ['拖拽', 'js'], intro: '<p>1、分割线宽度占位</p><p>2、左右两部分的最小宽度一致，可为0</p>', compatible: ['1', '1', '0', '', '1', '1', '', '1'], comMobile: ['', '', ''] },
     { id: '002', tit: '这是一条测试数据这是一条测试数据这是一条测试数据', type: '其他', keyWord: ['测试', '测试'], intro: '<p>dd</p>', compatible: ['', '1', '1', '0', '', '1', '1', '0'], comMobile: ['', '1', '0'] },
 ];
