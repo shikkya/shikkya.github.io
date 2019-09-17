06027f74cba4196e0bc3e87acdab22dccf7d095b
zhengy <zhengy@jufaanli.com>
2019-09-17 14:30:25 +0800

    a

---
+++ mobile/script/tool/index.js
@@ -0,0 +1,40 @@
+﻿/**
+ * tool - index
+ * @authors shikkya
+ * @date    2019-09-16
+ * @version $Id$
+ */
+
+// 工具列表
+var ToolItem = {
+    props: ['item'],
+    template: '<li><img src="../../images/tool_01.png" :data-src="\'../../images/\' + item.img"/><h3>{{item.tit}}</h3><a class="btn" @click="handleItemClick">下载</a></li>',
+    methods: {
+        handleItemClick: function() {
+            alert(this.item.url);
+        }
+    }
+}
+
+var vm = new Vue({
+    el: '#app',
+    components: {
+        ToolItem: ToolItem
+    },
+    data: {
+        tit: '便易工具 - SHIKKYA .',
+        text: '更多精彩请使用电脑浏览器打开！',
+        toolList: [
+            { img: 'tool_02.png', tit: '在线量尺', url: './ruler.html' },
+            { img: 'tool_03.png', tit: '圆周率速算', url: './pi.html' },
+            { img: 'tool_04.png', tit: '日期各种算', url: '' }, // ./date.html
+            { img: 'tool_05.png', tit: '专治选择困难症', url: '' }, // ./random.html
+            { img: 'tool_06.png', tit: '倒数计时器', url: './countdown.html' },
+            { img: 'tool_07.png', tit: '计时秒表', url: './stopwatch.html' }
+        ]
+    },
+    mounted: function() {
+        // 加载图片
+        loadImg();
+    }
+})
