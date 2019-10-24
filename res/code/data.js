bca0107ff29af6d7038a25d50802e65a369e4a3c
shikkya <48462635+shikkya@users.noreply.github.com>
2019-10-24 17:30:25 +0800

    a

--- res/code/data.js
+++ res/code/data.js
@@ -11,7 +11,7 @@ var codeList = [{
         tit: '容器内拖拽分割线自由分配左右空间',
         type: '功能',
         keyWord: ['拖拽', 'Js'],
-        compatible: ['1', '1', '0', '', '1', '1', '', '1'],
+        compatible: ['1', '1', '0', '', '1', '1', '0', '1'],
         comMobile: ['0', '', '0'],
         intro: '<p>1、分割线宽度占位</p><p>2、左右两部分的最小宽度一致，可为0</p>',
         bugIntro: ''
@@ -21,17 +21,17 @@ var codeList = [{
         tit: '数字滚动增值插件countUp.js',
         type: '插件,功能',
         keyWord: ['数字', '滚动', '跳数', 'Js'],
-        compatible: ['1', '1', '1', '', '1', '1', '', '1'],
+        compatible: ['1', '1', '1', '', '1', '1', '1', '1'],
         comMobile: ['1', '', '1'],
         intro: '<p>CountUp.js 无依赖的、轻量级的 JavaScript 类，可以用来快速创建以一种更有趣的动画方式显示数值数据。</p><p>换值只用于临时变化，重置开始后依然是初始值。</p><p>可配置的参数：</p><p>target = 目标元素的 ID</p><p>startVal = 开始值</p><p>endVal = 结束值</p><p>decimals = 小数位数，默认值是0</p><p>duration = 动画延迟秒数，默认值是2</p>',
-        bugIntro: ''
+        bugIntro: '<p>IE8支持初始化数字滚动，所有按钮功能均无效。</p>'
     },
     {
         id: '003',
         tit: '纯CSS3卡通蜡烛动画特效',
         type: '特效',
         keyWord: ['动画', 'CSS3'],
-        compatible: ['1', '1', '0', '', '1', '1', '', '1'],
+        compatible: ['1', '1', '0', '', '1', '1', '0', '1'],
         comMobile: ['1', '', '1'],
         intro: '',
         bugIntro: ''
@@ -41,7 +41,7 @@ var codeList = [{
         tit: 'textarea实时文字计数器',
         type: '功能',
         keyWord: ['文字', '计数', 'Js'],
-        compatible: ['1', '1', '1', '', '1', '1', '', '1'],
+        compatible: ['1', '1', '1', '', '1', '1', '0', '1'],
         comMobile: ['1', '', '1'],
         intro: '<p>过滤空格和换行，超出时数字标红。</p>',
         bugIntro: ''
@@ -51,7 +51,7 @@ var codeList = [{
         tit: '多边形彩色折纸动态圆球js特效动画',
         type: '特效',
         keyWord: ['Canvas', 'Js'],
-        compatible: ['1', '1', '0', '', '1', '1', '', '1'],
+        compatible: ['1', '1', '0', '', '1', '1', '0', '1'],
         comMobile: ['1', '', '1'],
         intro: '',
         bugIntro: ''
@@ -61,7 +61,7 @@ var codeList = [{
         tit: '可被鼠标划破的网',
         type: '特效',
         keyWord: ['Canvas', 'Js'],
-        compatible: ['1', '1', '1', '', '1', '1', '', '1'],
+        compatible: ['1', '1', '1', '', '1', '1', '0', '1'],
         comMobile: ['0', '', '0'],
         intro: '',
         bugIntro: '<p>safari浏览器下速度略迟缓，不影响使用。</p>'
@@ -71,7 +71,7 @@ var codeList = [{
         tit: '纯前端导出数据为Excel表格',
         type: '功能',
         keyWord: ['Excel', 'Js'],
-        compatible: ['1', '1', '0', '', '1', '1', '', '0'],
+        compatible: ['1', '1', '0', '', '1', '1', '0', '0'],
         comMobile: ['0', '', '0'],
         intro: '',
         bugIntro: '<p>第一种方法无法合并单元格，长字符串无法自动调整单元格宽度，导出格式只能为csv。</p><p>第二种方法导出后会有错误提示，但不影响使用。</p>'
@@ -91,7 +91,7 @@ var codeList = [{
         tit: '纯CSS3模拟动态水效果',
         type: '特效',
         keyWord: ['动画', 'CSS3'],
-        compatible: ['1', '1', '', '', '', '', '', ''],
+        compatible: ['1', '1', '', '', '1', '1', '0', ''],
         comMobile: ['', '', ''],
         intro: '',
         bugIntro: ''
