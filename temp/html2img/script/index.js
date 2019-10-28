6d6f136999763b77c155d6b6caea46998b9ec45e
shikkya <48462635+shikkya@users.noreply.github.com>
2019-10-28 15:56:38 +0800

    a

---
+++ temp/html2img/script/index.js
@@ -0,0 +1,62 @@
+/**
+ * index.html
+ * @authors shikkya
+ * @date    2019-10-28
+ * @version $Id$
+ */
+
+function toPdf() {
+    html2canvas(document.getElementById('wrap'), {
+        dpi: 200,
+        background: "#fff",
+        width: 192,
+        height: 108,
+        onrendered: function(canvas) {
+            document.body.appendChild(canvas);
+            console.log(saveAsPNG(canvas));
+            // window.open(saveAsPNG(canvas));
+            // document.getElementById('abtn').setAttribute('src', saveAsPNG(canvas));
+
+            downloadFile('ship.png', canvas.toDataURL("image/png"));
+        },
+    });
+}
+
+// 保存成png格式的图片
+function saveAsPNG(canvas) {
+    return canvas.toDataURL("image/png");
+}
+
+// 保存成jpg格式的图片
+function saveAsJPG(canvas) {
+    return canvas.toDataURL("image/jpeg");
+}
+
+// 保存成bmp格式的图片  目前浏览器支持性不好
+function saveAsBMP(canvas) {
+    return canvas.toDataURL("image/bmp");
+}
+
+function base64Img2Blob(code) {
+    var parts = code.split(';base64,');
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+    var uInt8Array = new Uint8Array(rawLength);
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+    return new Blob([uInt8Array], { type: contentType });
+}
+
+function downloadFile(fileName, content) {
+    var aLink = document.createElement('a');
+    var blob = base64Img2Blob(content); //new Blob([content]);
+
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("click", false, false); //initEvent 不加后两个参数在FF下会报错
+    aLink.download = fileName;
+    aLink.href = URL.createObjectURL(blob);
+
+    aLink.dispatchEvent(evt);
+}
