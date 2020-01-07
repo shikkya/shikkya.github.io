9443674011bcb3e54687f4457d6f0d693738b7c4
shikkya <48462635+shikkya@users.noreply.github.com>
2020-01-07 10:19:10 +0800

    Update data.js

--- res/code/data.js
+++ res/code/data.js
@@ -122,7 +122,7 @@ var codeList = [{
         type: '其他',
         keyWord: ['省市区', '三级联动', 'jQuery'],
         compatible: ['1', '1', '0', '', '1', '1', '0', ''],
-        comMobile: ['', '', ''],
+        comMobile: ['1', '', ''],
         intro: '<p>省市区数据更新于2019年12月11日</p>',
         bugIntro: '<p>三个列表滚动条位置无法重置</p>'
     }
