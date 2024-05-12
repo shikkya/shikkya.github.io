e26a17d719d49665596159a19909fdabecf7be4a
shikkya <crystal568@126.com>
2024-05-12 17:09:11 +0800

    a

--- tool/birthDate/js/index.js
+++ tool/birthDate/js/index.js
@@ -14,63 +14,67 @@ $(function () {
     this.createEvent = function () {
 
         // 计算预产期
-        $('#searchBirthDateBtn').on('click', function () {
-            if ($('#startDate').val().trim() == '') {
+        $('#calcBirthDateBtn').on('click', function () {
+            if ($('#lastDate').val().trim() == '') {
                 showMsg('请选择末次月经日期');
                 return false;
             }
-            self.searchBirthDate();
+            self.calcBirthDate();
         })
 
         // 计算产假结束期
-        $('#searchWorkDateBtn').on('click', function () {
-            if ($('#birthDate').val().trim() == '') {
+        $('#calcEndDateBtn').on('click', function () {
+            if ($('#startDate').val().trim() == '') {
                 showMsg('请选择预产期');
                 return false;
             }
-            self.searchWorkDate();
+            self.calcEndDate();
         })
     }
 
     // 计算预产期
-    this.searchBirthDate = function () {
-        var startDate = $('#startDate').val().trim();
+    this.calcBirthDate = function () {
+        var lastDate = $('#lastDate').val().trim();
         var produceDay = parseInt($('#produceDay').text().trim());
 
-        var dateObj = new Date(startDate);
+        var dateObj = new Date(lastDate);
         dateObj = new Date(dateObj.setDate(dateObj.getDate() + produceDay));
 
-        $('#birthDate').val(
+        $('#birthDate').text(
+            dateObj.getFullYear() + '年' + self.formatNum(dateObj.getMonth() + 1) + '月' + self.formatNum(dateObj.getDate()) + '日 星期' + weekArr[dateObj.getDay()]
+        );
+
+        $('#startDate').val(
             dateObj.getFullYear() + '-' + self.formatNum(dateObj.getMonth() + 1) + '-' + self.formatNum(dateObj.getDate())
         );
 
-        self.searchWorkDate();
+        self.calcEndDate();
     }
 
     // 计算产假结束期
-    this.searchWorkDate = function () {
-        var birthDate = $('#birthDate').val().trim();
+    this.calcEndDate = function () {
+        var startDate = $('#startDate').val().trim();
         var restDay = parseInt($('#restDay').text().trim());
 
-        var dateObj = new Date(birthDate);
+        var dateObj = new Date(startDate);
         dateObj = new Date(dateObj.setDate(dateObj.getDate() + restDay));
 
-        $('#workDate').text(
+        $('#endDate').text(
             dateObj.getFullYear() + '年' + self.formatNum(dateObj.getMonth() + 1) + '月' + self.formatNum(dateObj.getDate()) + '日 星期' + weekArr[dateObj.getDay()]
         );
     }
 
     // 初始化日历插件
     this.initDcalendar = function () {
+        $('#lastDate').dcalendarpicker({
+            format: 'yyyy-mm-dd',
+            language: 'ch'
+        });
         $('#startDate').dcalendarpicker({
             format: 'yyyy-mm-dd',
             language: 'ch'
         });
-        $('#birthDate').dcalendarpicker({
-            format: 'yyyy-mm-dd',
-            language: 'ch'
-        });
-        $('#startDate').val(new Date().getFullYear() + '-' + self.formatNum(new Date().getMonth() + 1) + '-' + self.formatNum(new Date().getDate()));
+        $('#lastDate').val(new Date().getFullYear() + '-' + self.formatNum(new Date().getMonth() + 1) + '-' + self.formatNum(new Date().getDate()));
     }
 
     // 格式化数字
@@ -84,7 +88,7 @@ $(function () {
     this.init = function () {
         self.createEvent();
         self.initDcalendar();
-        self.searchBirthDate();
+        self.calcBirthDate();
     }
 
     self.init();
